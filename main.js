13807d393d41bee06b9249b0924a736bb5600839
arinjayj1234 <arinjay@pingu.us>
2022-03-17 18:18:54 -0700

    Created Canvas

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("not empty");
+songa = ""
+songb = ""
+
+function preload(){
+	songa = loadSound("songa.mp3");
+	songb = loadSound("songb.mp3")	
+}
+
+function setup(){
+	canvas = createCanvas(600, 500);	
+	canvas.center()
+
+	video = createCapture(VIDEO);
+	video.hide();
+}
+
+function draw(){
+	image(0, 0, 600, 500);
+}
